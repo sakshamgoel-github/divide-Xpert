0424e82949b8a2777f24131f220475d4283ce28f
sakshamgoel-github <saksham.goel0808@gmail.com>
2023-08-05 17:16:12 +0530

    Backend Login

--- server/models/user.js
+++ server/models/user.js
@@ -22,6 +22,12 @@ const userSchema = new Schema({
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   groups: { type: [Schema.Types.ObjectId], ref: "Group" },
+  
+  password:{
+    type:String,
+    required:true
+  }
+  
 });
 
 module.exports = mongoose.model("User", userSchema);
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,27 +1,62 @@
 const express = require("express");
 const User = require("../models/user");
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const protect = require("../authMiddleware");
 const router = express.Router();
 
-router.get("/user",async (req,res) => {
+router.get("/user", async (req, res) => {
   try {
-    const users =await User.find();
+    const users = await User.find();
     res.json(users);
-} catch (error) {
-    res.status(500).json({message:error.message});
-}
-})
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.get("/user/:id", getUser, (req, res) => {
-  res.json(res.user);
+router.get("/me",protect,(req, res) => {
+  res.json(req.user);
 });
 
 router.post("/register", async (req, res) => {
-  const { name, email, groups } = req.body;
-  const newUser = new User({ name, email, groups });
+  const { name, email, password } = req.body;
   try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ name, email, password: hashedPassword });
     const user = await newUser.save();
-    res.status(201).json(user);
+
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      groups: user.groups,
+      token:generateJWT(user._id)
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return res.status(200).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        groups: user.groups,
+        token:generateJWT(user._id)
+      });
+    }
+    return res.status(400).json({ message: "Invalid user credentials" });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -39,6 +74,12 @@ async function getUser(req, res, next) {
   }
   res.user = user;
   next();
+}
+
+const generateJWT = (id) => {
+  return jwt.sign({id},process.env.JWT_SECRET,{
+    expiresIn:'30d'
+  })
 }
 
 module.exports = router;
